77f3800d960da20472898a14e0d2687264e80bd6
elkira01 <elkira01@gmail.com>
2026-03-15 16:06:27 +0100

    feat(admin): page level loading effect

---
+++ apps/admin/app/(global)/loading.tsx
@@ -0,0 +1,9 @@
+import { PulseLoader } from 'react-spinners';
+
+export default function Loading() {
+  return (
+    <div className="bg-gra flex h-full w-full items-center justify-center">
+      <PulseLoader color="#9ca3af" />
+    </div>
+  );
+}
