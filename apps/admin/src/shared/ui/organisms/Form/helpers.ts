0ebd36e54fd6c4e54b7ed9a97871103089b0a1a5
elkira01 <elkira01@gmail.com>
2026-03-17 14:45:43 +0100

    feat: create package form

--- apps/admin/src/shared/ui/organisms/Form/helpers.ts
+++ apps/admin/src/shared/ui/organisms/Form/helpers.ts
@@ -1,9 +1,12 @@
 import { EnumHelper } from '@/shared/lib/types';
 
-export const extractFormPayload = <TData = any>(formData: FormData): TData => {
-  const payload = Object.fromEntries(formData.entries()) as TData;
+export const extractFormPayload = <TData = any>(formData: FormData, skip: string[] = []): TData => {
+  const payload = Object.fromEntries(formData.entries()) as Record<string, unknown>;
 
-  return payload;
+  return skip.reduce((acc, key) => {
+    delete acc[key];
+    return acc;
+  }, payload) as TData;
 };
 
 export const parseEnumOptions = <TEnum extends object>(enumType: TEnum) => {
