e40d3056693d8fc7106caf9d99e520a535e04de0
elkira01 <elkira01@gmail.com>
2026-03-20 02:24:11 +0100

    feat(admin): delete confirmation modal

---
+++ apps/admin/src/shared/ui/organisms/modals/DeleteConfirmationModal.tsx
@@ -0,0 +1,44 @@
+import { Trash2Icon } from 'lucide-react';
+import { Button } from '../../atoms/Button';
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '../../atoms/Dialog';
+
+export const DeleteConfirmationModal = (props: {
+  open: boolean;
+  content: React.ReactNode;
+  onConfirm: any;
+  onOpenChange: (open: boolean) => void;
+}) => {
+  return (
+    <>
+      <Dialog open={props.open} onOpenChange={props.onOpenChange}>
+        <DialogContent className="flex flex-col gap-12 overflow-hidden bg-gray-100 p-5 md:max-w-sm">
+          <DialogHeader className="h-fit">
+            <DialogTitle className="hidden"></DialogTitle>
+          </DialogHeader>
+
+          <div className="flex flex-col items-center justify-center gap-y-6">
+            <div className="rounded-lg border bg-white p-6">
+              <Trash2Icon className="h-8 w-8 text-red-500" />
+            </div>
+            <div>{props.content}</div>
+          </div>
+
+          <DialogFooter className="border-t border-dashed border-gray-200">
+            <div className="flex w-full gap-x-3">
+              <Button
+                variant="secondary"
+                className="w-full"
+                onClick={() => props.onOpenChange(false)}
+              >
+                Annuler
+              </Button>
+              <Button variant="destructive" className="w-full" onClick={props.onConfirm}>
+                Confirmer
+              </Button>
+            </div>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
