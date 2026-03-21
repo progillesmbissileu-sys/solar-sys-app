3d0a44a984fce17f65eaebffe16283279ba6a46c
elkira01 <elkira01@gmail.com>
2026-03-21 14:35:06 +0100

    Remove debug console log from admin layout

--- apps/admin/app/(global)/layout.tsx
+++ apps/admin/app/(global)/layout.tsx
@@ -9,7 +9,6 @@ export default async function Layout({ children }: { children: React.ReactNode }
   const defaultOpen = cookieStore.get('sidebar:state')?.value === 'true';
 
   const response = await checkIdentityAction();
-  console.log({ response: JSON.stringify(response, null, 2) });
 
   if (!response.success) {
     if ([401, 403].includes(response.error.status)) {
